3fb7c0e46a2aa4ab4ac9e83aa58f9046dc52f9ee
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-02-06 18:48:19 +0200

    task-01 updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import Profile from '../components/Profile';
+import user from '../user.json';
+
+export default function App() {
+  return <div>
+    <Profile
+      username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} followers={user.stats.followers} views={user.stats.views} likes={user.stats.likes}
+    />
+  </div>;
 };
