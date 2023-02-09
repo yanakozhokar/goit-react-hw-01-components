7b87e89b27c4fa6fa6c485c7a597364a61e76fbf
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-02-09 10:45:08 +0200

    fixed

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,10 +2,6 @@ import PropTypes from 'prop-types';
 import css from 'components/FriendListItem/FriendListItem.module.css';
 
 export const FriendListItem = ({ id, avatar, name, isOnline }) => {
-  console.log(id);
-  console.log(avatar);
-  console.log(name);
-  console.log(isOnline);
   return (
     <li className={css.item} key={id}>
       <span className={css.status}></span>
